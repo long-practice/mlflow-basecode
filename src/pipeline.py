234bd6506b7296911d349ddca8d91c943b53127c
llong <jdkim96@gmail.com>
2023-10-02 14:27:39 +0900

    Modify Pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,7 +3,10 @@ import pickle
 
 import numpy as np
 import pandas as pd
+from xgboost import XGBRegressor, XGBClassifier
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 from preprocessing.preprocess import Preprocess
 from utils.model_tuning import Objective
 
@@ -13,8 +16,8 @@ class Pipeline():
         self.test = test_data
         self.model = None
 
-        self.valid_regressor_models = ['XGBoost', 'LightGBM', 'CatBoost', 'Linear Regression', 'Random Forest']
-        self.valid_classifier_models = ['XGBoost', 'LightGBM', 'CatBoost', 'Logistic Regression', 'Random Forest']
+        # self.valid_regressor_models = ['XGBoost', 'LightGBM', 'CatBoost', 'Linear Regression', 'Random Forest']
+        # self.valid_classifier_models = ['XGBoost', 'LightGBM', 'CatBoost', 'Logistic Regression', 'Random Forest']
 
     def preprocess(self):
         self.preprocess = Preprocess(self.train, self.test)
@@ -27,16 +30,23 @@ class Pipeline():
         return mdl
 
     def model_tuning(self, model_name):
-        if model_name not in self.valid_models:
-            print(f'{model_name} is not included in {self.valid_models}')
-            print('Check the working category (Regression or Classification)')
-            return None
+        # if model_name not in self.valid_regressor_models:
+        #     print(f'{model_name} is not included in {self.valid_models}')
+        #     print('Check the working category (Regression or Classification)')
+        #     return None
 
         if model_name == 'XGBoost':
-            ## optuna tuning
+            xgb_mdl = XGBRegressor()
+            X, y = self.train.drop('Strength', axis=1), self.train['Strength']
+            X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
+            print(X_train.shape, y_train.shape, X_valid.shape, y_valid.shape)
+            err_func = lambda y_true, y_pred: mean_squared_error(y_true, y_pred, squared=False)
+            xgb_obj = Objective(xgb_mdl, None, X_train, X_valid, y_train, y_valid, err_func, 'minimize', 10)
+            xgb_obj.study()
 
 
     def run(self):
         self.preprocess()
+        self.model_tuning('XGBoost')
 
-        self.model = self.get_model()
+        # self.model = self.get_model()
--- utils/model_tuning.py
+++ utils/model_tuning.py
@@ -1,10 +1,12 @@
+import os
+import pickle
+
 import optuna
 
 
 class Objective():
     def __init__(self, model, params, X_train, X_test, y_train, y_test, error_function, direction, n_trials):
         self.model = model
-        self.params = params
 
         self.X_train = X_train
         self.y_train = y_train
@@ -17,6 +19,18 @@ class Objective():
         self.n_trials = n_trials
 
     def __call__(self, trial):
+        self.params =  {
+            'max_depth': trial.suggest_int('max_depth', 1, 20),
+            'learning_rate': trial.suggest_float('learning_rate', 0.001, 1.0),
+            'n_estimators': trial.suggest_int('n_estimators', 500, 1000),
+            'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
+            'gamma': trial.suggest_float('gamma', 0.01, 1.0),
+            'subsample': trial.suggest_float('subsample', 0.01, 1.0),
+            'colsample_bytree': trial.suggest_float('colsample_bytree', 0.01, 1.0),
+            'reg_alpha': trial.suggest_float('reg_alpha', 0.01, 1.0),
+            'reg_lambda': trial.suggest_float('reg_lambda', 0.01, 1.0),
+        }
+        self.model.set_params(**self.params)
         self.model.fit(self.X_train, self.y_train)
         y_pred = self.model.predict(self.X_test)
 
@@ -25,4 +39,12 @@ class Objective():
 
     def study(self):
         study = optuna.create_study(direction=self.direction)
-        study.optimize(self, n_trials=100)
+        study.optimize(self, n_trials=self.n_trials)
+
+        best_params = study.best_params
+        self.model.set_params(**best_params)
+
+        model_name = 'XGB_model'
+        with open(f'./artifact/{model_name}.pkl', 'wb') as f:
+            pickle.dump(self.model, f)
+
